56b9e94f9b86d3a0c69d44655cb47e4eb0fb09a3
Ishan <magardeishan@gmail.com>
2024-08-11 00:32:36 +0530

    creating form

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from "react";
 
 const Profile = () => {
   return (
-    <div className="p-3 max-w-lg max-auto">
-        <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
+    <div className="flex items-center justify-center ">
+      
+      <form className="my-12 p-3">
+      <h1 className="text-3xl py-6 font-semibold text-center">Profile</h1>
+        <div className="flex flex-col gap-4">
+          <input
+            type="text"
+            id="Username"
+            placeholder="Username"
+            className="bg-slate-300 rounded-lg p-4"
+          />
+          <input
+            type="email"
+            id="email"
+            placeholder="E-mail"
+            className="bg-slate-300 rounded-lg p-4"
+          />
+          <input
+            type="password"
+            id="Password"
+            placeholder="Password"
+            className="bg-slate-300 rounded-lg p-4"
+          />
+          <button>
+            
+          </button>
+        </div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
